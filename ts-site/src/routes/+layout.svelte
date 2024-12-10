bd9d7d2d68ab5457e822e7a8f5cbc7dc2a1e5b8b
jonas.walravens <jonas.walravens@mediahuis.be>
2024-12-10 18:13:56 +0100

    Add initial styles and layout, including navigation and logo

--- ts-site/src/routes/+layout.svelte
+++ ts-site/src/routes/+layout.svelte
@@ -1,10 +1,29 @@
 <script>
-  let { children } = $props();
+	import '../app.css';
+	let { children } = $props();
 </script>
 
 <nav>
-  <a href="/">home</a>
-  <a href="/lineup">lineup</a>
+	<a href="/">Home</a>
+	<a href="/lineup">Lineup</a>
+	<a href="/tickets">Tickets</a>
 </nav>
 
 {@render children()}
+
+<style>
+	nav {
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 1.2rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 32px 0 0 0;
+	}
+
+	nav a {
+		color: white;
+		text-decoration: none;
+		margin: 0 10px;
+	}
+</style>
--- ts-site/src/routes/+page.svelte
+++ ts-site/src/routes/+page.svelte
@@ -1,3 +1,23 @@
+<script>
+	import Logo from '$lib/assets/amf_logo_white.png';
+</script>
+
 <div>
-  <h1>AMF</h1>
+	<main>
+		<img class="amf-logo" src={Logo} alt="Antwerp Metal Fest Logo" />
+	</main>
 </div>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: start;
+		height: 100svh;
+	}
+
+	.amf-logo {
+		width: 100%;
+		max-width: 440px;
+	}
+</style>
